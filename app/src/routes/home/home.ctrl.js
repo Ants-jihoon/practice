05e6d809a0431dd904cbafe8d21775f16ee78b2a
jihoon Shin <wlgnstls0413@naver.com>
2022-12-08 01:12:12 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,47 @@
 "use strict";
 
-const home = (req, res) => {
-    res.render("home/index");
+const output ={
+    home : (req, res) => {
+        res.render("home/index");
+    },
+    
+    login : (req, res) => {
+        res.render("home/login");
+    },
+    
+}
+
+const users = {
+    id:["wlgnstls0413" , "aabb", "abcd"],
+    psword:["0413", "0219", "1234"],
 };
 
-const login = (req, res) => {
-    res.render("home/login");
+
+
+const process ={
+    login : (req, res) => {
+        const id = req.body.id,
+            psword = req.body.psword;
+
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+        
+            if(users.psword[idx] === psword){
+                return res.json({
+                    success: true,
+                });
+            };
+        };
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        });
+    },
+
 };
 
 module.exports = {
-    home,
-    login,
+    output,
+    process,
 };
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -5,8 +5,9 @@ const router = express.Router();
 
 const ctrl = require("./home.ctrl");
 
-router.get("/", ctrl.home );
+router.get("/", ctrl.output.home );
+router.get("/login", ctrl.output.login );
+router.post("/login", ctrl.process.login );
 
-router.get("/login", ctrl.login );
 
 module.exports = router; //외부파일에서 사용할 수 있도록 던지기
