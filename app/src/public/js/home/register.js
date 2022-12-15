3e0270460ff422cc0150150316e454a440755a08
jihoon Shin <wlgnstls0413@naver.com>
2022-12-16 01:40:02 +0900

    winston 프로젝트에 적용

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -13,7 +13,7 @@ registerBtn.addEventListener("click",register);
 
 function register() {
     if(!id.value) return alert("아이디를 입력하시오");
-    if(!psword.value) return alert("비밀번호를 입력하시오.");
+    if(psword.value !== confirmPsword.value) return alert("비밀번호가 일치하지 않습니다.");
     
     const req= {
         id: id.value,
